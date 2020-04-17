676c3f580508e0ef01211daf1f65336fd14f8bc9
John Zakkam <ced18i059@iiitdm.ac.in>
2020-04-17 12:34:43 +0530

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8" />
-    <script src="../build/react.development.js"></script>
-    <script src="../build/react-dom.development.js"></script>
-    <script src="../build/babel.min.js"></script>
-  </head>
-  <body>
-    <div id="example"></div>
-    <script type="text/babel">
 
      class HelloMessage extends React.Component {
   render() {
@@ -24,9 +13,3 @@ ReactDOM.render(
   <HelloMessage name="John Zakkam" />,
   document.getElementById('hello-example')
 );
- 
-
-    </script>
-  </body>
-</html>
-
