706e078b4124be648ab8eac599a1f8fae4a31655
John Zakkam <ced18i059@iiitdm.ac.in>
2020-04-17 12:29:47 +0530

    Create helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,14 @@
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <HelloMessage name="John Zakkam" />,
+  document.getElementById('hello-example')
+);
