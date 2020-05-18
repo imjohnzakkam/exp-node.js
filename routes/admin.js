1c6ecab4ed24781c54aee06b9c8747710528004e
imjohnzakkam <johnzakkam2592@gmail.com>
2020-05-19 00:25:19 +0530

    u

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/add-product', (req, res, next) => {
+router.get('/add-product', (req, res, next) => {
 	res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>');
 });
 
-router.get('/product',(req, res, next) => {
+router.post('/product',(req, res, next) => {
 	console.log(req.body);
 	res.redirect('/');
 });
