a825d5faefd388e2775999a7e125e1a1c243550d
imjohnzakkam <johnzakkam2592@gmail.com>
2020-05-19 00:26:48 +0530

    u

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-	res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>');
+	res.send(
+		'<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>'
+	);
 });
 
 router.post('/product',(req, res, next) => {
