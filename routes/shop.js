713962d5dc6cd9219876877251dbd8e976b8d179
imjohnzakkam <johnzakkam2592@gmail.com>
2020-05-19 21:37:45 +0530

    u

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
 	const products = adminData.products;
-	res.render('shop', {prods : products});
+	res.render('shop', {prods : products, docTitle: 'Shop'});
 });
 
 module.exports = router;
