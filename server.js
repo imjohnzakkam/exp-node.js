df1fb2b3c11f3456a37e6df4f00da0a454b5bece
imjohnzakkam <johnzakkam2592@gmail.com>
2020-05-20 00:45:41 +0530

    u

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require("./routes/admin");
@@ -18,7 +20,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404');
+  res.status(404).render('404', {pageTitle: 'Page not found'});
 });
 
 app.listen(3000);
